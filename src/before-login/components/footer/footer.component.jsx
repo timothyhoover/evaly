1b9def6ec19850c9345f019d3683631f9b07bbaa
timothyhoover <timmchoover@gmail.com>
2021-04-07 16:46:41 -0500

    added footer component design

--- src/before-login/components/footer/footer.component.jsx
+++ src/before-login/components/footer/footer.component.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import sprite from '../../assets/sprite.svg';
 import './footer.styles.scss';
 
 const Footer = () => (
@@ -6,6 +7,27 @@ const Footer = () => (
 		<div className="footer__item-1">
 			<p> A company by Topic Talk Inglés</p>
 			<p>Copyright Evaly 2021 All rights reserved</p>
+		</div>
+
+		<div className="footer__item-2">
+			<p className="footer__logo"> Evaly</p>
+			<svg className="icon footer__fb-icon">
+				<use href={sprite + '#facebook'}></use>
+			</svg>
+			<svg className="icon footer__instagram-icon">
+				<use href={sprite + '#instagram'}></use>
+			</svg>
+		</div>
+
+		<div className="footer__item-3">
+			<ul>
+				<li>
+					<a href="#">Terms & Conditions</a>
+				</li>
+				<li>
+					<a href="#">Privacy Policy</a>
+				</li>
+			</ul>
 		</div>
 	</div>
 );
