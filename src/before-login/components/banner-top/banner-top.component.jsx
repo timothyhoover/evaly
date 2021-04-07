4b329725ec34fe7f6333d1e9a5fb4df217b15a43
timothyhoover <timmchoover@gmail.com>
2021-04-06 21:27:14 -0500

    added new banner component

---
+++ src/before-login/components/banner-top/banner-top.component.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import BannerBorder from '../../assets/high-res/banner-border.png';
+import './banner-top.styles.scss';
+
+const BannerTop = () => (
+	<div>
+		<div className="banner-top">
+			<div className="banner-top__item">
+				<h2>Our Method</h2>
+				<p>
+					We evaluate your level based on real world exam situations
+					that give an outcome close to what you might see on the
+					Cambridge exam.
+				</p>
+			</div>
+		</div>
+	</div>
+);
+
+export default BannerTop;
--- src/before-login/pages/home/homepage.component.jsx
+++ src/before-login/pages/home/homepage.component.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import MainHero from '../../components/main-hero/main-hero.component';
+import BannerTop from '../../components/banner-top/banner-top.component';
 import './homepage.styles.scss';
 
 const HomePage = () => (
 	<div className="homepage">
 		<MainHero />
+		<BannerTop />
 	</div>
 );
 
